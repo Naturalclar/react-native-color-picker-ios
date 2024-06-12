69e2d9d1fa2763f0a0668a9791a73cb3bd2c25f5
Jesse Katsumata <jesse.katsumata@gmail.com>
2024-06-12 09:25:11 +0900

    chore: format

--- example/react-native.config.js
+++ example/react-native.config.js
@@ -1,16 +1,17 @@
 const project = (() => {
   try {
-    const { configureProjects } = require("react-native-test-app");
+    const { configureProjects } = require('react-native-test-app');
     return configureProjects({
       android: {
-        sourceDir: "android",
+        sourceDir: 'android',
       },
       ios: {
-        sourceDir: "ios",
+        sourceDir: 'ios',
       },
       windows: {
-        sourceDir: "windows",
-        solutionFile: "windows/# OR: npx init-test-app# ✔ What is the name of your test app? … WebviewExample# ✔ Which platforms do you need test apps for? › Android, iOS, macOS, Windows# ✔ Where should we create the new project? … example.sln",
+        sourceDir: 'windows',
+        solutionFile:
+          'windows/# OR: npx init-test-app# ✔ What is the name of your test app? … WebviewExample# ✔ Which platforms do you need test apps for? › Android, iOS, macOS, Windows# ✔ Where should we create the new project? … example.sln',
       },
     });
   } catch (_) {
