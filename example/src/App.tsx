ba3964d647d8129d1df344e4cda09402a1ceebf8
Jesse Katsumata <jesse.katsumata@gmail.com>
2024-11-18 13:07:34 +0900

    Use biome instead of eslint and prettier (#616)

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,68 +1,68 @@
-import * as React from 'react';
-import { useState } from 'react';
+import * as React from "react";
+import { useState } from "react";
 import {
-  View,
-  StyleSheet,
-  SafeAreaView,
-  TouchableOpacity,
-  Text,
-  useColorScheme,
-} from 'react-native';
-import ColorPicker from 'react-native-color-picker-ios';
+	View,
+	StyleSheet,
+	SafeAreaView,
+	TouchableOpacity,
+	Text,
+	useColorScheme,
+} from "react-native";
+import ColorPicker from "react-native-color-picker-ios";
 
 export default function App() {
-  const colorScheme = useColorScheme();
+	const colorScheme = useColorScheme();
 
-  const [selectedColor, setSelectedColor] = useState(
-    colorScheme === 'dark' ? '#ddd' : '#000'
-  );
-  const handlePress = () => {
-    ColorPicker.showColorPicker(
-      { supportsAlpha: true, initialColor: selectedColor },
-      (color) => {
-        setSelectedColor(color);
-      }
-    );
-  };
+	const [selectedColor, setSelectedColor] = useState(
+		colorScheme === "dark" ? "#ddd" : "#000",
+	);
+	const handlePress = () => {
+		ColorPicker.showColorPicker(
+			{ supportsAlpha: true, initialColor: selectedColor },
+			(color) => {
+				setSelectedColor(color);
+			},
+		);
+	};
 
-  const textStyle = React.useMemo(
-    () => ({
-      color: colorScheme === 'dark' ? '#ddd' : '#000',
-    }),
-    [colorScheme]
-  );
-  return (
-    <SafeAreaView style={styles.container}>
-      <TouchableOpacity onPress={handlePress} style={styles.textContainer}>
-        <Text style={textStyle}>Open Color Picker</Text>
-      </TouchableOpacity>
-      <View style={styles.textContainer}>
-        <Text>Currently selected color:</Text>
-      </View>
-      <View
-        style={[
-          styles.selectedColorContainer,
-          {
-            backgroundColor: selectedColor,
-          },
-        ]}
-      />
-    </SafeAreaView>
-  );
+	const textStyle = React.useMemo(
+		() => ({
+			color: colorScheme === "dark" ? "#ddd" : "#000",
+		}),
+		[colorScheme],
+	);
+	return (
+		<SafeAreaView style={styles.container}>
+			<TouchableOpacity onPress={handlePress} style={styles.textContainer}>
+				<Text style={textStyle}>Open Color Picker</Text>
+			</TouchableOpacity>
+			<View style={styles.textContainer}>
+				<Text>Currently selected color:</Text>
+			</View>
+			<View
+				style={[
+					styles.selectedColorContainer,
+					{
+						backgroundColor: selectedColor,
+					},
+				]}
+			/>
+		</SafeAreaView>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  textContainer: {
-    marginBottom: 8,
-  },
-  selectedColorContainer: {
-    width: 40,
-    height: 40,
-    borderRadius: 8,
-  },
+	container: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	textContainer: {
+		marginBottom: 8,
+	},
+	selectedColorContainer: {
+		width: 40,
+		height: 40,
+		borderRadius: 8,
+	},
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,63 +1,63 @@
-import { ColorValue, NativeModules, processColor } from 'react-native';
+import { type ColorValue, NativeModules, processColor } from "react-native";
 
 /**
  * Type of option used in JS side
  */
 type ColorPickerInputOptions = {
-  /**
-   * whether alpha is supported or not.
-   * if true, user can chose the opacity of the color
-   * @default false
-   */
-  supportsAlpha?: boolean;
-  /**
-   * initial color displayed on the picker
-   */
-  initialColor?: ColorValue;
+	/**
+	 * whether alpha is supported or not.
+	 * if true, user can chose the opacity of the color
+	 * @default false
+	 */
+	supportsAlpha?: boolean;
+	/**
+	 * initial color displayed on the picker
+	 */
+	initialColor?: ColorValue;
 };
 
 /**
  * Actual options to be passed to Native Module
  */
 type ColorPickerNativeOptions = {
-  supportsAlpha?: boolean;
-  initialColor?: number | symbol | null;
+	supportsAlpha?: boolean;
+	initialColor?: number | symbol | null;
 };
 
 type ColorPickerMethods = {
-  showColorPicker: (
-    /**
-     * options for color picker
-     */
-    options?: ColorPickerNativeOptions,
-    /**
-     * callback method once color is chosen
-     */
-    callback?: (color: string) => void
-  ) => void;
+	showColorPicker: (
+		/**
+		 * options for color picker
+		 */
+		options?: ColorPickerNativeOptions,
+		/**
+		 * callback method once color is chosen
+		 */
+		callback?: (color: string) => void,
+	) => void;
 };
 
 const { RNCColorPicker } = NativeModules as {
-  RNCColorPicker: ColorPickerMethods;
+	RNCColorPicker: ColorPickerMethods;
 };
 
 const ColorPicker = {
-  showColorPicker: (
-    options?: ColorPickerInputOptions,
-    callback?: (color: string) => void
-  ) => {
-    const convertedOptions = {
-      ...options,
-      initialColor: options?.initialColor
-        ? processColor(options.initialColor)
-        : undefined,
-    };
+	showColorPicker: (
+		options?: ColorPickerInputOptions,
+		callback?: (color: string) => void,
+	) => {
+		const convertedOptions = {
+			...options,
+			initialColor: options?.initialColor
+				? processColor(options.initialColor)
+				: undefined,
+		};
 
-    RNCColorPicker.showColorPicker(
-      convertedOptions,
-      callback ? callback : () => {}
-    );
-  },
+		RNCColorPicker.showColorPicker(
+			convertedOptions,
+			callback ? callback : () => {},
+		);
+	},
 };
 
 export default ColorPicker;
